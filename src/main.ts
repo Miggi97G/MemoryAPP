b835709dd5e11b63e5602e6fb591f381045d7a10
Miggi97G <m.famgarcia@outlook.com>
2026-08-28 15:13:57 +0200

    Added card style

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
 import "./style/styles.scss";
+
+function init() {
+    const fieldRef = document.getElementById("field");
+    if (fieldRef) {
+        fieldRef.addEventListener("click", e => {
+            const card = (e.target as HTMLElement).closest(".card") as HTMLButtonElement;
+            if (card) {
+                card.classList.toggle("is-flipped");
+            }
+        })
+    }
+}
+
+init();
